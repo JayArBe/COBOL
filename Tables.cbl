9f4b2717d2a729af97c4cc1a1d51a30374b87953
JayArBe <janbloecher@gmail.com>
2024-01-30 12:33:32 +0100

    Tables

---
+++ Tables.cbl
@@ -0,0 +1,39 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       ENVIRONMENT DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       CONFIGURATION SECTION.
+      *-----------------------
+       INPUT-OUTPUT SECTION.
+      *-----------------------
+       DATA DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       FILE SECTION.
+      *-----------------------
+       WORKING-STORAGE SECTION.
+       01 SALES-TABLE.
+         03 BRANCH-NO OCCURS 4.
+           05 MONTHLY-SALES PIC A(10) VALUE 2
+           OCCURS 4 PIC 9V9.
+      *-----------------------
+       PROCEDURE DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       MAIN-PROCEDURE.
+            DISPLAY BRANCH-NO(1).
+            DISPLAY BRANCH-NO(2).
+            DISPLAY BRANCH-NO(3).
+            DISPLAY BRANCH-NO(4).
+
+            DISPLAY MONTHLY-SALES(1,1).
+
+
+            STOP RUN.
+
+       END PROGRAM YOUR-PROGRAM-NAME.
