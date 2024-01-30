a4fc9f42119208357a0b1070f2dfede075ca44b4
JayArBe <janbloecher@gmail.com>
2024-01-30 09:41:35 +0100

    Finished MOVE TO lesson starting on printing

--- MoveTo.cbl
+++ MoveTo.cbl
@@ -20,13 +20,16 @@
       *-----------------------
        WORKING-STORAGE SECTION.
        01 TEST-IDENTIFIER-1 PIC 99.
-       01 TEST-IDENTIFIER-2 PIC 99.
+       01 TEST-IDENTIFIER-2 PIC S99 SIGN LEADING SEPARATE.
        01 TEST-IDENTIFIER-3 PIC X(12).
       *each digit is converted to binary when saving
        01 TEST-BINARY PIC 9(3) VALUE 123 USAGE IS BINARY.
        01 TEST-DEC PIC 9(3) VALUE 123 USAGE IS PACKED-DECIMAL.
       *DISPLAY is default. each digit is saved seperately + sign
        01 TEST-DISPLAY PIC 9(4) VALUE 123 USAGE IS DISPLAY.
+       01 TEST-FLOAT PIC 9V999 VALUE 1.004.
+       01 TEST-DATE PIC 99/99/99 VALUE 300124.
+       01 TEST-DATE2 PIC 99B99B99 VALUE 300124.
       *-----------------------
        PROCEDURE DIVISION.
       *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
@@ -58,6 +61,7 @@
 
            DISPLAY TEST-IDENTIFIER-3.
 
+      *TEST-IDENTIFIER-3 can only hold 12 signs so all others are cut off
            MOVE 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' TO TEST-IDENTIFIER-3
 
            DISPLAY TEST-IDENTIFIER-3.
@@ -74,6 +78,10 @@
            GIVING TEST-DISPLAY
 
            DISPLAY TEST-DISPLAY.
+
+           DISPLAY TEST-FLOAT.
+
+           DISPLAY TEST-DATE.
 
            STOP RUN.
        END PROGRAM MoveTo.
